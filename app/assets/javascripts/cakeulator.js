d7cfc8918cd470a85601373eee6451c426f7c9bf
Daniela Wellisz <daniela.wellisz@gmail.com>
2013-03-04 12:05:30 -0800

    sliders change the height of the 3-D cake now

--- app/assets/javascripts/cakeulator.js
+++ app/assets/javascripts/cakeulator.js
@@ -1,45 +1,56 @@
-(function($) {
+(function ($) {
   var $scope = $('body.cakeulator');
-  var $tiers = $scope.find('.tiers.straight > div');
+  var $tiers = $scope.find('.tiers > div');
   var $lis = $scope.find('ol li');
 
-  var multiplyOne = function($item, proportion) {
+  var multiplyOne = function ($item, proportion, callback) {
     var origHeight = $item.height();
     $item.height(origHeight * proportion);
+    if (callback) { callback($item, origHeight); }
   };
 
-  var multiplyCollection = function($collection,proportion){
-    $collection.each(function(iterator, item){
-      multiplyOne($(item), proportion)
+  var multiplyEach = function ($collection,proportion,callback){
+    $collection.each(function (iterator, item){
+      multiplyOne($(item), proportion, callback);
     })
   };
 
-  var tierHeightSwitch = function(tierName) {
-    $scope.find('input#' + tierName).change(function(e) {
-      var ratio = $scope.find('input#' + tierName).val() == "4" ? 4/5 : 5/4;
-      multiplyOne($scope.find('.tiers.straight .' + tierName), ratio);
-    });
+  var moveTop = function ($item, distance) {
+    var origTop = parseInt($item.css('top'));
+    $item.css('top',(origTop + distance) + 'px');
   };
 
-  //Too late at night for for loops
+  var tierHeightSwitch = function (tierName) {
+    $scope.find('input#' + tierName).change(function (e) {
+      var ratio = $scope.find('input#' + tierName).val() === "4" ? 4/5 : 5/4;
+      var $tier = $scope.find('.tiers .' + tierName);
+      var origHeight = $tier.height();
+      multiplyOne($tier, ratio);
+      moveTop($tier.find('.cakebottom'), $tier.height() - origHeight);
+    });
+
+  };
+
   tierHeightSwitch('first');
   tierHeightSwitch('second');
   tierHeightSwitch('third');
 
-  $scope.find('input#spacing').change(function(e) {
+  $scope.find('input#spacing').change(function (e) {
     $lis.css('margin-bottom',e.target.value + 'px')
   });
 
-  $scope.find('input#unit').change(function(e) {
+  $scope.find('input#unit').change(function (e) {
     var unitLength = $lis.height();
     var proportion = e.target.valueAsNumber / unitLength;
-    multiplyCollection($lis,proportion);
+    multiplyEach($lis,proportion);
   });
 
-  $scope.find('input#tiers').change(function(e) {
+  $scope.find('input#tiers').change(function (e) {
     ratio = $scope.find('input#first').val() == 4 ? 4 : 5;
     var unitLength = $tiers.height() / ratio;
     var proportion = e.target.valueAsNumber / unitLength;
-    multiplyCollection($tiers,proportion);
+    multiplyEach($tiers,proportion, function ($tier, origHeight) {
+      moveTop($tier.find('.cakebottom'), $tier.height() - origHeight);
+    });
   });
 })($);
