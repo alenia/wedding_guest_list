0d6e2bfeaf39cf14f264e12b6ea310a4ef661d99
Daniela Wellisz <daniela.wellisz@gmail.com>
2013-03-04 19:37:50 -0800

    refactored straddlers into jQuery object; some delinting

--- app/assets/javascripts/cakeulator.js
+++ app/assets/javascripts/cakeulator.js
@@ -1,88 +1,99 @@
 (function ($) {
-  var $scope, $tiers, $lis;
+  var $scope, $tiers, $lis, transitions, collections, actions;
+
   $scope = $('body.cakeulator');
   $tiers = $scope.find('.tiers > div');
   $lis = $scope.find('ol li');
 
-  var transitions = {
+  transitions = {
     multiplyOne: function ($item, proportion, callback) {
       var origHeight = $item.height();
       $item.height(origHeight * proportion);
       if (callback) { callback(origHeight, $item); }
     },
 
-    multiplyEach: function ($collection,proportion,callback){
+    multiplyEach: function ($collection, proportion, callback) {
       var self = this;
-      $collection.each(function (index, item){
+      $collection.each(function (index, item) {
         self.multiplyOne($(item), proportion, callback);
-      })
+      });
     },
 
     moveTop: function ($item, distance) {
       var origTop =  parseInt($item.css('top'));
-      $item.css('top',(origTop + distance) + 'px');
-    },
+      $item.css('top', (origTop + distance) + 'px');
+    }
   };
+
+  collections = {
+    getTierBoundaries: function () {
+      var tierBoundaries = [];
+      $tiers.each(function (index, tier) {
+        var tierTop = $(tier).offset().top;
+        tierBoundaries.push(tierTop);
+        tierBoundaries.push(tierTop + $(tier).height());
+      });
+
+      return _(tierBoundaries).uniq();
+    },
+
+    getTierStraddlers: function () {
+      var tierStraddlers = $('');
+      $lis.each(function (index, li) {
+        var top, bottom;
+        top = $(li).offset().top;
+        bottom = top + $(li).height();
+        _(collections.getTierBoundaries()).each(function (num) {
+          if ((bottom > num) && (num > top)) {
+            tierStraddlers = tierStraddlers.add(li);
+          }
+        });
+      });
+      return tierStraddlers;
+    }
+  };
+
 
   var markTierStraddlers = function () {
-    $lis.css('background-color','rgba(0,0,255,.3)')
-
-    var tierBoundaries = [];
-    $tiers.each(function (index, tier) {
-      var tierTop = $(tier).offset().top;
-      tierBoundaries.push(tierTop);
-      tierBoundaries.push(tierTop + $(tier).height());
-    });
-
-    tierBoundaries = _(tierBoundaries).uniq();
-
-    $lis.each(function (index, li) {
-      var top, bottom;
-      top = $(li).offset().top;
-      bottom = top + $(li).height();
-      _(tierBoundaries).each( function (num) {
-        if ((bottom > num) && (num > top)) {
-          $(li).css('background-color','rgba(255,0,0,.5)')
-        }
-      })
-    });
+    $lis.css('background-color', 'rgba(0,0,255,.3)');
+    collections.getTierStraddlers().css('background-color', 'rgba(255,0,0,.5)');
   };
 
-  var actions = {
-    scaleTiers: function(value) {
-      ratio = $scope.find('input#first').val() == 4 ? 4 : 5;
-      var unitLength = $tiers.height() / ratio;
-      var proportion = value / unitLength;
-      transitions.multiplyEach($tiers,proportion, function (origHeight, $tier) {
+  actions = {
+    scaleTiers: function (value) {
+      var ratio, unitLength, proportion;
+      ratio = $scope.find('input#first').val() === 4 ? 4 : 5;
+      unitLength = $tiers.height() / ratio;
+      proportion = value / unitLength;
+      transitions.multiplyEach($tiers, proportion, function (origHeight, $tier) {
         transitions.moveTop($tier.find('.cakebottom'), $tier.height() - origHeight);
       });
       markTierStraddlers();
     },
 
     //essentially a toggle even if height is 4 or 5
-    toggleTierHeight: function($tier, value) {
-      var ratio = value === "4" ? 4/5 : 5/4;
-      transitions.multiplyOne($tier, ratio, function (origHeight){
+    toggleTierHeight: function ($tier, value) {
+      var ratio = value === "4" ? 4 / 5 : 5 / 4;
+      transitions.multiplyOne($tier, ratio, function (origHeight) {
         transitions.moveTop($tier.find('.cakebottom'), $tier.height() - origHeight);
       });
       markTierStraddlers();
     },
 
-    toggleFibSpacing: function(isChecked) {
+    toggleFibSpacing: function (isChecked) {
       this.isFibSpacing = isChecked;
       // TODO: actions should not be responsible for fetching form values
       this.scaleStripeMargin($scope.find('input#spacing').val());
       markTierStraddlers();
     },
 
-    scaleStripes: function(value) {
-      var unitLength = $lis.height();
-      var proportion = value / unitLength;
-      transitions.multiplyEach($lis,proportion);
+    scaleStripes: function (value) {
+      var proportion = value / $lis.height();
+      transitions.multiplyEach($lis, proportion);
       markTierStraddlers();
     },
 
-    scaleStripeMargin: function(value) {
+    scaleStripeMargin: function (value) {
       if (this.isFibSpacing) {
         this._fibScaleStripeMargin(value);
       } else {
@@ -93,34 +104,34 @@
 
     //private
 
-    _linearScaleStripeMargin: function(value) {
-      $lis.css('margin-bottom', value + 'px')
+    _linearScaleStripeMargin: function (value) {
+      $lis.css('margin-bottom', value + 'px');
     },
 
-    _fibScaleStripeMargin: function(value) {
+    _fibScaleStripeMargin: function (value) {
       var proportion = value / $lis.height();
-      $lis.each(function (iterator, li){
+      $lis.each(function (iterator, li) {
         var height = $(li).height();
-        $(li).css('margin-bottom', (height * proportion) + 'px')
+        $(li).css('margin-bottom', (height * proportion) + 'px');
       });
     }
   };
 
-  var createUrl = function(root) {
+  var createUrl = function (root) {
     var url, params;
     params = {};
-    _(['spacing','stripes','tiers']).each(function (inputName){
+    _(['spacing', 'stripes', 'tiers']).each(function (inputName) {
       params[inputName] = $scope.find('input#' + inputName).val();
     });
     params['fib_spacing'] = $scope.find('input#fib_spacing').prop('checked');
 
-    _(['first','second','third']).each(function (tierName) {
+    _(['first', 'second', 'third']).each(function (tierName) {
       params[tierName + '_tier'] = $scope.find('input[name="' + tierName + '"]:checked').val();
     });
 
     url = root + '?';
     _(params).each(function (value, key) {
-      url += key + '=' + value + '&'
+      url += key + '=' + value + '&';
     });
     return url;
   }
@@ -145,18 +156,18 @@
     actions.scaleTiers(e.target.valueAsNumber);
   });
 
-  _(['first','second','third']).each(function (tierName) {
+  _(['first', 'second', 'third']).each(function (tierName) {
     var $trigger = $scope.find('input[name="' + tierName + '"]');
     $trigger.change(function (e) {
       actions.toggleTierHeight($scope.find('.tiers .' + tierName), e.currentTarget.value);
     });
   });
 
-  _(['spacing','stripes','tiers','fib_spacing']).each(function (inputName){
+  _(['spacing', 'stripes', 'tiers', 'fib_spacing']).each(function (inputName) {
     $scope.find('input#' + inputName).change();
   });
 
-  _(['first','second','third']).each(function (tierName) {
+  _(['first', 'second', 'third']).each(function (tierName) {
     //TODO: make less hacky. this includes fixing the hacky toggleTierHeight method.
     $trigger = $scope.find('input[name="' + tierName + '"]:checked');
     if ($trigger.val() === '5') {
